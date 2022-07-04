f2607b45e4effae60b28290c5f090af81992f131
vidhichadha2507 <vidhichadha2507@gmail.com>
2022-07-04 23:31:04 +0530

    footer link added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
    <p className="footer-heart">
         Made with <g-emoji className="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
-          <img className="emoji" alt="heart" height={20} width={20} src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png" /></g-emoji> by <a href="https://github.com/vidhichadha2507/restaurant-website">Vidhi Chadha</a>
+          <img className="emoji" alt="heart" height={20} width={20} src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png" /></g-emoji> by <a href="https://github.com/vidhichadha2507/restaurant-website" target="_blank">Vidhi Chadha</a>
       </p>
   )
 }
