32cc5ac53572acc28d8a7febb22d49a6001cfd9c
Shruti0208 <86532235+Shruti0208@users.noreply.github.com>
2022-10-03 14:55:55 +0530

    Add files via upload
    JavaScript File

---
+++ exercise.js
@@ -0,0 +1,9 @@
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+	alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+	alert("Powering On. Enjoy the ride!");
+} else if (Number(age) === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
